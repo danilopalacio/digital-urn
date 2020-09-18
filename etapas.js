03e94872cdfaf86e8d4405bfe0125f5318bf0415
Danilo Palacio <70732176+danilopalacio@users.noreply.github.com>
2020-09-17 22:44:06 -0300

    Add files via upload
    Electronic urn project.

---
+++ etapas.js
@@ -0,0 +1,104 @@
+let etapas= [
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '45',
+                nome: 'Felicio Ramuth',
+                partido: 'PSDB',
+                fotos:[
+                    {url: 'felicio.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '30',
+                nome:  'Agliberto Chagas',
+                partido: 'NOVO',
+                fotos:[
+                    {url: 'agliberto.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '65',
+                nome: 'João Bosco',
+                partido:  'PCdoB',
+                fotos:[
+                    {url: 'joaobosco.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Coronel Nikoluk',
+                partido: 'PL',
+                fotos:[
+                    {url: 'coronelnikoluk.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '40',
+                nome: 'Dr Cury',
+                partido:  'PSB',
+                fotos:[
+                    {url: 'drcury.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '55',
+                nome: 'Renata Paiva',
+                partido: 'PSD',
+                fotos:[
+                    {url: 'renatapaiva.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Letícia Aguiar',
+                partido:  'PSL',
+                fotos:[
+                    {url: 'leticiaaguiar.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '50',
+                nome: 'Marina Sassi',
+                partido: 'PSOL',
+                fotos:[
+                    {url: 'marinasassi.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '16',
+                nome: 'Raquel de Paula',
+                partido: 'PSTU',
+                fotos:[
+                    {url: 'raqueldepaula.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Wagner Balieiro',
+                partido: 'PT',
+                fotos:[
+                    {url: 'wagnerbalieiro.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '36',
+                nome: 'Luiz Carlos de Oliveira',
+                partido: 'PTC',
+                fotos:[
+                    {url: 'luizcarlosdeoliveira.jpeg', legenda: 'Prefeito'}
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Shakespeare',
+                partido: 'Republicanos',
+                fotos:[
+                    {url: 'shakespeare.jpeg', legenda: 'Prefeito'}
+                ]
+            }
+        ]    
+    }                     
+]
